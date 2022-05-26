032fbd1929b721c5afa8123e2567c9b25a5a31ec
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-26 13:15:37 +0300

    feat: header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <div class="header__wrapper container">
+      <AppIcon class="header__logo" iconName="IconLogo"></AppIcon>
+      <nav class="header__nav">
+        <ul class="header__nav-list">
+          <li class="header__nav-item">
+            <router-link
+              class="header__nav-link"
+              active-class="header__nav-link--active"
+              to="/"
+            >
+              Поиск
+            </router-link>
+          </li>
+          <li class="header__nav-item">
+            <router-link
+              class="header__nav-link"
+              active-class="header__nav-link--active"
+              to="/favorites"
+            >
+              Избранное
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="header__logout">
+        <router-link class="header__logout-link" to="/">Выйти</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import AppIcon from '@/components/AppIcon.vue';
+
+export default {
+  name: 'AppHeader',
+  components: { AppIcon },
+};
+</script>
+
+<style scoped>
+
+</style>
