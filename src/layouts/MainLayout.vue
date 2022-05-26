43d4ab10a42fabdac9cfb63bdca7d99c55ee352a
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-26 13:17:23 +0300

    feat: main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  name: 'MainLayout',
+  components: { AppHeader },
+};
+</script>
+
+<style scoped>
+
+</style>
