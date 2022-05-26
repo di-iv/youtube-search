c49a3df0186e3587a2d07cd671029f6358cc004f
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-26 12:58:53 +0300

    feat: icon component

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,19 @@
+<template>
+ <component :is="iconName"></component>
+</template>
+
+<script>
+import IconLogo from '@/components/icons/IconLogo.vue';
+
+export default {
+  name: 'AppIcon',
+  components: {
+    IconLogo,
+  },
+  props: {
+    iconName: {
+      type: String,
+    },
+  },
+};
+</script>
