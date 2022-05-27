1e6e242adcfb192f5c7d5612e4385ccac268bf04
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-27 18:24:35 +0300

    feat: button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="btn"
+    :class="[buttonSize, {'btn--fill': isFill, 'btn--group': isGroup}]"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Кнопка',
+    },
+    isFill: {
+      type: Boolean,
+      default: true,
+    },
+    size: {
+      type: String,
+      default: 'small',
+    },
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    buttonSize() {
+      return `btn--${this.size}`;
+    },
+  },
+};
+</script>
