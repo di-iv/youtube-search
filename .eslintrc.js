704159ba47bc5e05e90b247f52e15396d944d555
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-27 15:01:43 +0300

    build: add rules to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/extensions': 'off',
+    'vuejs-accessibility/label-has-for': ['error', {
+      required: {
+        some: ['nesting', 'id'],
+      },
+    }],
   },
 };
