db7d4d401b41874d76edcd2930ddb5bc36136bcd
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-27 22:36:21 +0300

    feat: modal card

---
+++ src/components/ModalCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="modal-card">
+    <div class="modal-card__modal" :class=className>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalCard',
+  props: {
+    className: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
