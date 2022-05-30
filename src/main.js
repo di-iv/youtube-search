9327f39b2aff35bb9d4da1f30ba5fa3fa10dd605
di-iv <67899051+di-iv@users.noreply.github.com>
2022-05-30 11:47:11 +0300

    style: remove eslint-disable

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// eslint-disable-next-line import/no-unresolved
 import './styles/index.scss';
 
 createApp(App).use(store).use(router).mount('#app');
