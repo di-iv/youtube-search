dd70d093f2084eeebce52d38be98ee5c8baa905d
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-02 12:19:23 +0300

    feat: modal component

---
+++ src/components/AppModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="modal">
+    <app-card>
+      <slot />
+    </app-card>
+  </div>
+</template>
+
+<script>
+import AppCard from '@/components/AppCard';
+
+export default {
+  name: 'AppModal',
+  components: { AppCard },
+};
+</script>
+
+<style scoped>
+
+</style>
