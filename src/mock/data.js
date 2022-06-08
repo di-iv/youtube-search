5d46d38345ddc9d79055cb9217f44d7818198e8c
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-08 15:43:03 +0300

    feat: mock data

---
+++ src/mock/data.js
@@ -0,0 +1,174 @@
+const data = [
+  {
+    kind: 'youtube#searchResult',
+    etag: 'N2BOk2Bchngbi_3Bl-iaPZw2wjA',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'SGbtGaLtMOY',
+    },
+    snippet: {
+      publishedAt: '2022-05-30T19:00:15Z',
+      channelId: 'UCo_q6aOlvPH7M-j_XGWVgXg',
+      title: 'MY WORST WIPEOUT AT THE WEDGE!',
+      description: "STAY PSYCHED MERCHANDISE: https://www.jamieobrienshop.com - JAMIE O'BRIEN SURF SCHOOL TURTLE BAY HAWAII ...",
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/SGbtGaLtMOY/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/SGbtGaLtMOY/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/SGbtGaLtMOY/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "Jamie O'Brien",
+      liveBroadcastContent: 'none',
+      publishTime: '2022-05-30T19:00:15Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'f0GFVHxQW5br3fqf9evhKCNqK5g',
+    id: {
+      kind: 'youtube#video',
+      videoId: '0nS9m_3qQXM',
+    },
+    snippet: {
+      publishedAt: '2022-05-30T17:38:52Z',
+      channelId: 'UCVo06dBGK9VBBhq15wJxZHQ',
+      title: '🔵 4k (ASMR) Teahupoo April/May 2022 - Waves of the World Surfing 🌊',
+      description: 'Filmed by Boris Martinet: https://www.instagram.com/bogus689. Official Waves of the World live stream! Fall asleep or just relax ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/0nS9m_3qQXM/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/0nS9m_3qQXM/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/0nS9m_3qQXM/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Chris Kincade Media - Waves of the World ',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-05-30T17:38:52Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: '2G9-EfoRQADJaAzhnt7U9G-qEmI',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'bJ1ReqDTHRQ',
+    },
+    snippet: {
+      publishedAt: '2022-05-30T11:00:15Z',
+      channelId: 'UCzcQOTuXYGuCvTekySb_CeQ',
+      title: 'Solid Swell For The Joe&#39;s - The Bukit',
+      description: "While the crowds flocked to the bigger name breaks during this swell, this spot on Bali's Bukit had clean lines and a bit more ...",
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/bJ1ReqDTHRQ/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/bJ1ReqDTHRQ/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/bJ1ReqDTHRQ/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Surfers of Bali',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-05-30T11:00:15Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: '3b0uAJv_I_QOfYbqUs7R4cryzLU',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'ma67yOdMQfs',
+    },
+    snippet: {
+      publishedAt: '2021-01-23T17:00:15Z',
+      channelId: 'UC--3c8RqSfAqYBdDjIG3UNA',
+      title: 'These Were The All-Time Surfing Moments Of The Year | Best Of 2020',
+      description: "Well, that was a weird ride. Though it hasn't been easy, at least when we fixed our gaze on the ocean — or favorite place in the ...",
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/ma67yOdMQfs/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/ma67yOdMQfs/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/ma67yOdMQfs/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Red Bull Surfing',
+      liveBroadcastContent: 'none',
+      publishTime: '2021-01-23T17:00:15Z',
+    },
+  },
+  {
+    kind: 'youtube#searchResult',
+    etag: 'T6fqOPUHfsOhZhJE2lLR7dVMOyU',
+    id: {
+      kind: 'youtube#video',
+      videoId: 'NhVNZO75gNc',
+    },
+    snippet: {
+      publishedAt: '2022-05-30T22:07:21Z',
+      channelId: 'UCFdhu2ZZ7GBbGliV8tZ0UJw',
+      title: 'POV SURF - HI MOROCCO (Episode 1)',
+      description: 'We recently have flown to Morocco with my friend There is going to be multipe episodes GoPro 8 2,7K 60i SuperView Instagram ...',
+      thumbnails: {
+        default: {
+          url: 'https://i.ytimg.com/vi/NhVNZO75gNc/default.jpg',
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: 'https://i.ytimg.com/vi/NhVNZO75gNc/mqdefault.jpg',
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: 'https://i.ytimg.com/vi/NhVNZO75gNc/hqdefault.jpg',
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: 'Kai Noa',
+      liveBroadcastContent: 'none',
+      publishTime: '2022-05-30T22:07:21Z',
+    },
+  },
+];
+
+export default data;
--- src/services/Search.js
+++ src/services/Search.js
@@ -1,14 +1,18 @@
 import api from '@/services/api';
+import data from '@/mock/data';
 
 export default class Search {
   static async searchRequest(request, resultsCount = 12) {
-    const results = await api.get('search', {
-      params: {
-        part: 'snippet',
-        maxResults: resultsCount,
-        q: request,
-      },
-    });
-    return results.data.items;
+    if (!data) {
+      const results = await api.get('search', {
+        params: {
+          part: 'snippet',
+          maxResults: resultsCount,
+          q: request,
+        },
+      });
+      return results.data.items;
+    }
+    return data;
   }
 }
