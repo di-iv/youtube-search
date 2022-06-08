02b2b7350b674435892540f07faccad0d6812fba
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-08 13:40:31 +0300

    feat: favourites page

--- src/views/FavouritesView.vue
+++ src/views/FavouritesView.vue
@@ -1,11 +1,57 @@
 <template>
-  <div class="container">
-    <h1>Избранное</h1>
-  </div>
+  <section class="favourites">
+    <div class="favourites__wrapper container">
+      <h1 class="favourites__title">
+        Избранное
+      </h1>
+      <div class="favourites__requests">
+        <div class="favourites__requests-item">
+          <p class="favourites__requests-item-name">
+            видео
+          </p>
+          <div class="favourites__requests-item-buttons">
+            <app-button
+              text="Изменить"
+              style-type="link"
+              color="primary"
+              class="favourites__requests-item-button"
+            />
+            <app-button
+              text="Удалить"
+              style-type="link"
+              color="danger"
+              class="favourites__requests-item-button"
+            />
+          </div>
+        </div>
+        <div class="favourites__requests-item">
+          <p class="favourites__requests-item-name">
+            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+          </p>
+          <div class="favourites__requests-item-buttons">
+            <app-button
+              text="Изменить"
+              style-type="link"
+              color="primary"
+              class="favourites__requests-item-button"
+            />
+            <app-button
+              text="Удалить"
+              style-type="link"
+              color="danger"
+              class="favourites__requests-item-button"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
+import AppButton from '@/components/AppButton';
 
 export default {
   name: 'FavouritesView',
+  components: { AppButton },
 };
 </script>
