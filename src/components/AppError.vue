1e779e51ea85114ed5984c7222f56809e2a19e54
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-09 15:04:07 +0300

    feat: error component

---
+++ src/components/AppError.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error">
+    <div class="error__message">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppError',
+  props: {
+    text: {
+      type: String,
+      default: null,
+      required: true,
+    },
+  },
+};
+</script>
