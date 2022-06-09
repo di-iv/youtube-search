58e64c3cb3b7438703c44ba9842da08173725805
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-09 20:40:02 +0300

    feat(modal-edit-fav): check uniq name and request

--- src/components/ModalEditFavourite.vue
+++ src/components/ModalEditFavourite.vue
@@ -11,6 +11,7 @@
       label="Запрос"
       class="modal-favorites__form-group"
       :is-valid="!v$.request.$error"
+      required
     />
     <AppInput
       id="name"
@@ -20,6 +21,7 @@
       class="modal-favorites__form-group"
       :is-valid="!v$.name.$error"
     />
+    <app-error :errors="errors" />
     <div class="modal-favorites__buttons">
       <AppButton
         text="Не изменять"
@@ -41,14 +43,17 @@
 
 <script>
 import AppButton from '@/components/AppButton';
+import AppError from '@/components/AppError';
 import AppInput from '@/components/AppInput';
 import AppModal from '@/components/AppModal';
+import Favourites from '@/services/Favourites';
 import useVuelidate from '@vuelidate/core';
 import { required } from '@vuelidate/validators';
 
 export default {
   name: 'ModalEditFavourite',
   components: {
+    AppError,
     AppButton,
     AppInput,
     AppModal,
@@ -57,6 +62,8 @@ export default {
     return {
       request: '',
       name: '',
+      errors: [],
+      id: '',
       v$: useVuelidate(),
     };
   },
@@ -70,16 +77,42 @@ export default {
       },
     };
   },
+  computed: {
+    favouritesExceptThisOne() {
+      const favourites = [...this.$store.state.favorites];
+      favourites.splice(this.id, 1);
+      return favourites;
+    },
+    isNameUniq() {
+      return Favourites.checkUniq(this.favouritesExceptThisOne, 'name', this.name);
+    },
+    isRequestUniq() {
+      return Favourites.checkUniq(this.favouritesExceptThisOne, 'request', this.request);
+    },
+  },
   methods: {
     async save() {
       const isFormValid = await this.v$.$validate();
-      if (isFormValid) {
+      if (isFormValid && this.isNameUniq && this.isRequestUniq) {
         this.$refs.modal.confirm();
+        this.errors = [];
+        return;
+      }
+      this.alertErrors();
+    },
+    alertErrors() {
+      this.errors = [];
+      if (!this.isRequestUniq) {
+        this.errors.push('Такой запрос уже существует');
+      }
+      if (!this.isNameUniq) {
+        this.errors.push('Такое имя уже существует');
       }
     },
     async open(data) {
       this.request = data.request;
       this.name = data.name;
+      this.id = Favourites.getIndex('request', this.request);
       const res = await this.$refs.modal.open();
       if (res) {
         return { request: this.request, name: this.name };
