4f1c0479d52cb6e4a6c6f1dd82d1c342f1f74331
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-14 17:46:06 +0300

    feat(Search): add order param

--- src/services/Search.js
+++ src/services/Search.js
@@ -2,17 +2,16 @@ import api from '@/services/api';
 import data from '@/mock/data';
 
 export default class Search {
-  static async searchRequest(request, resultsCount = 12) {
-    if (!data) {
-      const results = await api.get('search', {
-        params: {
-          part: 'snippet',
-          maxResults: resultsCount,
-          q: request,
-        },
-      });
-      return results.data.items;
-    }
-    return data;
+  static async searchRequest(request, order = null, resultsCount = 12) {
+    if (data) return data;
+    const results = await api.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: resultsCount,
+        order,
+        q: request,
+      },
+    });
+    return results.data.items;
   }
 }
