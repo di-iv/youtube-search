9c43e4dab400df214f4c1dc07bb23d8a537ea192
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-14 15:18:03 +0300

    feat: select

---
+++ src/components/InputRange.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-range">
+    <label
+      for="input"
+      class="input-range__label"
+    >Максимальное количество</label>
+    <div class="input-range__wrapper">
+      <input
+        :value="modelValue"
+        class="input-range__slider"
+        type="range"
+        min="0"
+        max="50"
+        @input="updateValue"
+      >
+      <input
+        :value="modelValue"
+        class="input-range__text"
+        type="number"
+        min="0"
+        max="50"
+        @input="updateValue"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputRange',
+  props: {
+    modelValue: {
+      type: Number,
+      default: 25,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue'],
+  methods: {
+    updateValue(e) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
