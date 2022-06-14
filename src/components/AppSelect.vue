de1e17cba70913ee0251c5bf3b56b0af6dc9d82d
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-14 15:14:49 +0300

    feat: select

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="select">
+    <label
+      for="select"
+      class="select__label"
+    >{{ label }}</label>
+    <select
+      class="select__select"
+      @change="updateValue"
+    >
+      <option
+        v-for="option in options"
+        :key="option"
+        :value="option.value"
+      >
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSelect',
+  props: {
+    label: {
+      type: String,
+      default: 'Сортировать по',
+      required: false,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue'],
+  methods: {
+    updateValue(e) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
