66970794478f871670a5b9615e1ebee16493ebf2
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-15 10:51:07 +0300

    fix: rename api

--- src/services/Search.js
+++ src/services/Search.js
@@ -1,10 +1,10 @@
-import api from '@/services/api';
+import { youtubeApi } from '@/services/api';
 import data from '@/mock/data';
 
 export default class Search {
   static async searchRequest(request, order = null, resultsCount = 12) {
     if (data) return data;
-    const results = await api.get('search', {
+    const results = await youtubeApi.get('search', {
       params: {
         part: 'snippet',
         maxResults: resultsCount,
