30c58afed0cb77fd17c247a3cbe38d7bf6dcdb4c
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-20 17:01:57 +0300

    feat: remove mock data

--- src/services/YouTube.js
+++ src/services/YouTube.js
@@ -1,9 +1,7 @@
 import { youtubeApi } from '@/services/api';
-import data from '@/mock/data';
 
 export default class YouTube {
   static async search(request, order = null, resultsCount = 12) {
-    if (data) return data;
     const results = await youtubeApi.get('search', {
       params: {
         part: 'snippet',
