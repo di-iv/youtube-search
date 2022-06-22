d28990e8cea2d38fc9b8c0796c25962245f846d7
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-22 15:00:51 +0300

    style: rearrange methods

--- src/entities/LocalStorage.js
+++ src/entities/LocalStorage.js
@@ -3,11 +3,11 @@ export default class LocalStorage {
     return JSON.parse(localStorage.getItem(key));
   }
 
-  static set(key, data) {
-    return localStorage.setItem(key, JSON.stringify(data));
-  }
-
   static remove(key) {
     return localStorage.removeItem(key);
+  }
+
+  static set(key, data) {
+    return localStorage.setItem(key, JSON.stringify(data));
   }
 }
