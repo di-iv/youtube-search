2213cb5cb41716f20a4d34d5b1c32a195e17a492
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-22 16:34:00 +0300

    docs(YouTube): jsdoc

--- src/entities/YouTube.js
+++ src/entities/YouTube.js
@@ -1,6 +1,13 @@
 import { youtubeApi } from '@/services/api';
 
 export default class YouTube {
+  /**
+   * Search YouTube videos
+   * @param {string} request - search request
+   * @param {string | null} order - videos sort order
+   * @param {number} resultsCount - amount of search results
+   * @returns {Promise<array>} - search results
+   */
   static async search(request, order = null, resultsCount = 12) {
     const searchResults = await youtubeApi.get('search', {
       params: {
