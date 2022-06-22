a57dd88c375326f3e6ce88155b31ccf3286c4685
di-iv <67899051+di-iv@users.noreply.github.com>
2022-06-22 15:00:30 +0300

    style: rearrange methods

--- src/entities/Favourites.js
+++ src/entities/Favourites.js
@@ -2,16 +2,6 @@ import LocalStorage from '@/entities/LocalStorage';
 import { getIndex, hasValue } from '@/utilities/helpers';
 
 export default class Favourites {
-  static #get() {
-    return LocalStorage.get('favourites') ?? [];
-  }
-
-  static save(newFavourite) {
-    const favourites = Favourites.#get();
-    favourites.push(newFavourite);
-    LocalStorage.set('favourites', favourites);
-  }
-
   static findIndexByRequestAndUserId(request, userId) {
     const favourites = Favourites.#get();
     return favourites.findIndex((el) => (
@@ -19,21 +9,18 @@ export default class Favourites {
     ));
   }
 
-  static update(idx, favourite) {
-    const favourites = Favourites.#get();
-    favourites[idx] = favourite;
-    LocalStorage.set('favourites', favourites);
-  }
-
-  static remove(idx) {
-    const favourites = Favourites.#get();
-    favourites.splice(idx, 1);
-    LocalStorage.set('favourites', favourites);
+  static #get() {
+    return LocalStorage.get('favourites') ?? [];
   }
 
   static getByUserId(userId) {
     const favourites = Favourites.#get();
     return favourites.filter((favourite) => favourite.userId === userId);
+  }
+
+  static getIndex(field, value, userId) {
+    const favourites = Favourites.getByUserId(userId);
+    return getIndex(favourites, field, value);
   }
 
   static isNameUnique(favourites, value) {
@@ -44,8 +31,21 @@ export default class Favourites {
     return !hasValue(favourites, 'request', value);
   }
 
-  static getIndex(field, value, userId) {
-    const favourites = Favourites.getByUserId(userId);
-    return getIndex(favourites, field, value);
+  static remove(idx) {
+    const favourites = Favourites.#get();
+    favourites.splice(idx, 1);
+    LocalStorage.set('favourites', favourites);
+  }
+
+  static save(newFavourite) {
+    const favourites = Favourites.#get();
+    favourites.push(newFavourite);
+    LocalStorage.set('favourites', favourites);
+  }
+
+  static update(idx, favourite) {
+    const favourites = Favourites.#get();
+    favourites[idx] = favourite;
+    LocalStorage.set('favourites', favourites);
   }
 }
